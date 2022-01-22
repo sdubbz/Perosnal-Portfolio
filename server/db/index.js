b2961093b266d6ad04b75a3d2937a2172dd26550
scott <scottw91@live.ca>
2022-01-21 22:37:06 -0500

    set up database

---
+++ server/db/index.js
@@ -0,0 +1,6 @@
+const Sequelize = require("sequelize");
+
+const dbConnection = new Sequelize( "postgres://localhost:5432/portfolio");
+
+module.exports = dbConnection;
+
--- server/index.js
+++ server/index.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const app = express();
 
-const port = 8080
-app.listen(port, () => {
-  console.log(`server is running on port ${port}`);
-})
+const port = 8080;
+
+const dbConnection = require("./db");
+
+
+const startServer = async () => {
+  await dbConnection.sync();
+
+  app.listen(port, () => {
+    console.log(`server is running on port ${port}`);
+  });
+};
+
+startServer();
+
+
 
 app.get("/", (req, res) => {
   res.send("hello")
