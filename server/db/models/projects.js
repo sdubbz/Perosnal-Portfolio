8a727a44ca653be52c85f3f89f36367480fca474
scott <scottw91@live.ca>
2022-01-26 18:41:47 -0500

    added default picture to projects table

--- server/db/models/projects.js
+++ server/db/models/projects.js
@@ -1,10 +1,15 @@
 const Sequelize = require("sequelize");
 const db = require("../index")
+const defaultPic = require("../../../default.jpg")
 
 
 const Project = db.define("project", {
   title:{
     type: Sequelize.STRING,
+  },
+  imageUrl:{
+    type: Sequelize.STRING,
+    defaultValue: defaultPic
   }
 });
 
